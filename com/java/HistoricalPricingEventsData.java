15bd5de75b028f6bf4d931b62f2ea3e82084839a
Pimchaya <pimchaya.wongrukun@thomsonreuters.com>
2019-07-08 15:55:59 +0700

    Update HistoricalPricingEventsData.java

--- com/java/HistoricalPricingEventsData.java
+++ com/java/HistoricalPricingEventsData.java
@@ -4,23 +4,26 @@ import java.util.HashMap;
 import java.util.Vector;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-
+//Jackson
 import com.fasterxml.jackson.annotation.*;
+//The following fields/properies in historical pricing event are ignored
 @JsonIgnoreProperties(value = {
 	    "universe",
 	    "adjustments",
 	    "defaultPricingField",
 	    "qos",
 	    "meta",
-	    "status"
+	    "status",
+	    "summaryTimestampLabel",
+	    "interval"
 	})
 
 public class HistoricalPricingEventsData {
-	//the map of name and type of each field
+	//the map keeping name and type of all fields
 	private LinkedHashMap<String, String> headers = new LinkedHashMap<String, String>();
-	//data - the list of fields' value
+	//the vector keeping the data of all events
 	private Vector<Vector<Object>> dataJson = new Vector<Vector<Object>>();
-	
+	//deserialize headers fields into the map
     @JsonProperty("headers")
 	private void unpackMultipleJsonObjects(Vector<HashMap<String, Object>> headersJson) {
     	Iterator<HashMap<String, Object>> fieldList = headersJson.iterator(); 
@@ -29,14 +32,15 @@ public class HistoricalPricingEventsData {
             headers.put(afield.get("name").toString(), afield.get("type").toString());
         } 
 	}
+  //get deserialized headers map
     public LinkedHashMap<String, String> getHeaders() {
     	return headers;
     }
+    //deserialize all events in data field into a vector of vector
+  	//return the vector
     @JsonProperty("data")
     public Vector<Vector<Object>>  getData() {
     	return dataJson;
     }
-    
-    
     
 }
